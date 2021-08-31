6d61551dbffc771d46c677c36824f57e2fb0d52c
sbr-vincent <vincent.g.sbr@gmail.com>
2021-08-31 16:00:03 -0700

    Added State

--- src/components/MyNewComponent.js
+++ src/components/MyNewComponent.js
@@ -3,13 +3,26 @@ import { Component } from 'react';
 
 
 class PersonCard extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            personAge: parseInt(this.props.age)
+        };
+    }
+
+    birthdayButton = () => {
+        console.log("Age Bruh");
+        this.setState({personAge: this.state.personAge + 1});
+    }
+
     render(){
-        const { firstName, lastName, age, hair_color } = this.props;
+        const { firstName, lastName, hair_color } = this.props;
         return(
             <div>
                 <h1>{lastName}, {firstName}</h1>
-                <p>Age: {age}</p>
+                <p>Age: {this.state.personAge} </p>
                 <p>Hair Color: {hair_color}</p>
+                <button onClick={ this.birthdayButton }>Birthday Button for {firstName} {lastName}</button>
             </div>
         )
     };
